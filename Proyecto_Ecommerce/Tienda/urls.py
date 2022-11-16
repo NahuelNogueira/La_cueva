f4e17508919978d70555112ac216e6c2779b4f35
NahuelNogueira <nahuel.nogueira93@hotmail.com>
2022-11-16 20:21:25 -0300

    Se agrega sistema de registro y Login

--- Proyecto_Ecommerce/Tienda/urls.py
+++ Proyecto_Ecommerce/Tienda/urls.py
@@ -1,11 +1,15 @@
 from django.contrib.auth import views
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
-from .views import inicio, catalogo,  login, signup
+
+from .views import inicio, catalogo, login_request, registro
+
 
 urlpatterns = [
     path('', inicio, name='Inicio'),
     path('catalogo', catalogo, name='Catalogo'),
-    path('login', login, name='Login'),
-    path('signup', signup, name='Signup'),
+    path('login', login_request, name='Login'),
+    path('registro', registro, name='Registro'),
+    path('logout', LogoutView.as_view(template_name="logout.html"), name="Logout"),
 ]
--- Proyecto_Ecommerce/Tienda/views.py
+++ Proyecto_Ecommerce/Tienda/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+
+from .forms import UserRegisterForm
 from Producto.models import Producto, Categoria
 
 # Create your views here.
@@ -8,11 +12,55 @@ def inicio(request):
     productos = Producto.objects.all()
     return render(request, 'inicio.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_request(request):
+    
+    if request.method == 'POST':
+            
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            data = form.cleaned_data
+            
+            usuario = data['username']
+            contra = data['password']
+            
+            user = authenticate(username=usuario, password=contra)
+            
+            if user:
+                
+                login(request, user)
+                
+                return render(request, "inicio.html", {"mensaje":f"Bienvenido {usuario}"})
+            
+            else:
+                
+                return render(request, "inicio.html", {"mensaje":"Error, datos incorrectos"})
+        
+        return render(request, "inicio.html", {"mensaje":"Error, formulario erroneo"})
+    
+    else:
+    
+        form = AuthenticationForm()
+    
+        return render(request, "login.html", {'form':form})
 
-def signup(request):
-    return render(request, 'signup.html')
+def registro(request):
+    
+    if request.method == "POST":
+        
+        form = UserRegisterForm(request.POST)
+        
+        if form.is_valid():
+            
+            username = form.cleaned_data['username']
+            form.save()
+            return render(request, 'inicio.html', {"mensaje":"Usuario Creado"})
+    
+    else:
+        form = UserRegisterForm()
+    
+        return render(request, "registro.html", {"form":form})
 
 def catalogo(request):
     
