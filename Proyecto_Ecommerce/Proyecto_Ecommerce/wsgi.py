bfc367a8c2903a3c61ac49f6586ff034064c280f
NahuelNogueira <nahuel.nogueira93@hotmail.com>
2023-05-16 20:57:03 -0300

    Settig up for deploy

--- Proyecto_Ecommerce/Proyecto_Ecommerce/wsgi.py
+++ Proyecto_Ecommerce/Proyecto_Ecommerce/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/Proyecto Ecommerce/Proyecto_Ecommerce')
+sys.path.append('/Proyecto Ecommerce/Proyecto_Ecommerce/Proyecto_Ecommerce')
 
 from django.core.wsgi import get_wsgi_application
 
